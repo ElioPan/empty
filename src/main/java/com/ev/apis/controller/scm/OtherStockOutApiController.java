1fcd9882643d3468203c6957e71bfd4d526e66cd
gmj <gumingjie.qi@gmail.com>
2020-03-03 10:44:01 +0800

    出库高级查询参数错误

--- src/main/java/com/ev/apis/controller/scm/OtherStockOutApiController.java
+++ src/main/java/com/ev/apis/controller/scm/OtherStockOutApiController.java
@@ -159,7 +159,7 @@ public class OtherStockOutApiController {
             // 高级查询
             @ApiParam(value = "业务类型") @RequestParam(value = "outboundType", defaultValue = "", required = false) Integer outboundType,
             @ApiParam(value = "规格型号") @RequestParam(value = "specification", defaultValue = "", required = false) String specification,
-            @ApiParam(value = "批次") @RequestParam(value = "materielName", defaultValue = "", required = false) String batch,
+            @ApiParam(value = "批次") @RequestParam(value = "batch", defaultValue = "", required = false) String batch,
             @ApiParam(value = "审核状态") @RequestParam(value = "auditSign", defaultValue = "", required = false) Long auditSign,
             @ApiParam(value = "出库员") @RequestParam(value = "operatorName", defaultValue = "", required = false) String operatorName,
             @ApiParam(value = "出库员Id") @RequestParam(value = "operator", defaultValue = "", required = false) Long operator,
@@ -222,7 +222,7 @@ public class OtherStockOutApiController {
             // 高级查询
             @ApiParam(value = "业务类型") @RequestParam(value = "outboundType", defaultValue = "", required = false) Integer outboundType,
             @ApiParam(value = "规格型号") @RequestParam(value = "specification", defaultValue = "", required = false) String specification,
-            @ApiParam(value = "批次") @RequestParam(value = "materielName", defaultValue = "", required = false) String batch,
+            @ApiParam(value = "批次") @RequestParam(value = "batch", defaultValue = "", required = false) String batch,
             @ApiParam(value = "审核状态") @RequestParam(value = "auditSign", defaultValue = "", required = false) Long auditSign,
             @ApiParam(value = "出库员") @RequestParam(value = "operatorName", defaultValue = "", required = false) String operatorName,
             @ApiParam(value = "出库员Id") @RequestParam(value = "operator", defaultValue = "", required = false) Long operator,
