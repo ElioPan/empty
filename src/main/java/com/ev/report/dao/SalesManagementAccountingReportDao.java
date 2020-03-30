36c63a47398f84cdfc030377128d82e3c65233be
gmj <gumingjie.qi@gmail.com>
2020-03-30 11:07:44 +0800

    销售合同，委外合同报表

---
+++ src/main/java/com/ev/report/dao/SalesManagementAccountingReportDao.java
@@ -0,0 +1,25 @@
+package com.ev.report.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 销售管理报表分析
+ *
+ * @author ev-monitor
+ * @email 286600136@qq.com
+ * @date 2020-03-18 17:22:16
+ */
+@Mapper
+public interface SalesManagementAccountingReportDao {
+
+    List<Map<String, Object>> salesContractList(Map<String, Object> params);
+
+    List<Map<String, Object>> debtDueList(Map<String, Object> params);
+
+    List<Map<String, Object>> summaryList(Map<String, Object> params);
+
+    List<Map<String, Object>> balanceList(Map<String, Object> params);
+}
---
+++ src/main/java/com/ev/report/service/SalesManagementAccountingReportService.java
@@ -0,0 +1,22 @@
+package com.ev.report.service;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 销售管理报表分析
+ *
+ * @author ev-monitor
+ * @email 286600136@qq.com
+ * @date 2020-03-18 17:22:16
+ */
+public interface SalesManagementAccountingReportService {
+
+    List<Map<String, Object>> salesContractList(Map<String, Object> params);
+
+    List<Map<String, Object>> debtDueList(Map<String, Object> params);
+
+    List<Map<String, Object>> summaryList(Map<String, Object> params);
+
+    List<Map<String, Object>> balanceList(Map<String, Object> params);
+}
