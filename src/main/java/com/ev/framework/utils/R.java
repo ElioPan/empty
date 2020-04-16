6eca681234ce2c0677f32c4d5d477c79041a669b
zd100710 <911435330@qq.com>
2020-04-16 09:43:15 +0800

    消息提示

--- src/main/java/com/ev/framework/utils/R.java
+++ src/main/java/com/ev/framework/utils/R.java
@@ -6,41 +6,43 @@ import java.util.Map;
 public class R extends HashMap<String, Object> {
 	private static final long serialVersionUID = 1L;
 
+	private static final String PREFIX = "芯软提示：";
+
 	public R() {
 		put("code", 0);
-		put("msg", "操作成功");
+		put("msg", PREFIX+"操作成功");
 	}
 
 	public static R error() {
-		return error(1, "操作失败");
+		return error(1, PREFIX+"操作失败");
 	}
 
 	public static R error(String msg) {
-		return error(500, msg);
+		return error(500, PREFIX+msg);
 	}
 
 	public static R everror(String msg) {
-		return error(-1, msg);
+		return error(-1, PREFIX+msg);
 	}
 
 	public static R error(int code, String msg) {
 		R r = new R();
 		r.put("code", code);
-		r.put("msg", msg);
+		r.put("msg", PREFIX+msg);
 		return r;
 	}
 
     public static R error(int code, String msg, Map<String, Object> map) {
         R r = new R();
         r.put("code", code);
-        r.put("msg", msg);
+        r.put("msg", PREFIX+msg);
         r.put("data", map);
         return r;
     }
 
 	public static R ok(String msg) {
 		R r = new R();
-		r.put("msg", msg);
+		r.put("msg", PREFIX+msg);
 		return r;
 	}
 
