fdc4c01cd79778f2c54f86d936a493d19a61d7d2
gmj <gumingjie.qi@gmail.com>
2020-04-24 17:05:56 +0800

    Mybatis 对批操作处理设置公共字段

--- src/main/java/com/ev/framework/config/MybatisInterceptor.java
+++ src/main/java/com/ev/framework/config/MybatisInterceptor.java
@@ -5,6 +5,7 @@ import org.apache.ibatis.executor.Executor;
 import org.apache.ibatis.mapping.MappedStatement;
 import org.apache.ibatis.mapping.SqlCommandType;
 import org.apache.ibatis.plugin.*;
+import org.apache.ibatis.session.defaults.DefaultSqlSession;
 import org.apache.shiro.ShiroException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,6 +13,7 @@ import org.springframework.stereotype.Component;
 
 import java.lang.reflect.Field;
 import java.util.Date;
+import java.util.List;
 import java.util.Properties;
 
 @Component
@@ -25,48 +27,24 @@ public class MybatisInterceptor implements Interceptor {
     private final static String FIELD_UPDATE_TIME = "updateTime";
     private final static String FIELD_DEL_FLAG = "delFlag";
 
+    @SuppressWarnings("unchecked")
     @Override
     public Object intercept(Invocation invocation) throws Throwable {
         try {
             Object parameter = invocation.getArgs()[1];
             Date now = new Date();
-            Class classParameter = (Class) parameter.getClass();
-            Field[] fields = classParameter.getDeclaredFields();
-            for (Field field : fields) {
-                field.setAccessible(true);
-                String fieldName = field.getName();
-                if (FIELD_CREATE_BY.equalsIgnoreCase(fieldName)) {
-                    Object value = field.get(parameter);
-                    if (value == null) {
-                        MappedStatement mappedStatement = (MappedStatement) invocation.getArgs()[0];
-                        SqlCommandType sqlCommandType = mappedStatement.getSqlCommandType();
-                        if (SqlCommandType.INSERT.equals(sqlCommandType)) {
-                            field.set(parameter, ShiroUtils.getUserId());
-                        }
+            Long userId = ShiroUtils.getUserId();
+            if (parameter instanceof DefaultSqlSession.StrictMap) {
+                DefaultSqlSession.StrictMap<Object> objectStrictMap =  (DefaultSqlSession.StrictMap<Object>)parameter;
+                Object objectList = objectStrictMap.get("list");
+                if(objectList instanceof List){
+                    List<Object> list = (List<Object>)objectList;
+                    for (Object o : list) {
+                        this.setFiled(o,invocation,now,userId);
                     }
-                }else if (FIELD_CREATE_TIME.equalsIgnoreCase(fieldName)) {
-                    Object value = field.get(parameter);
-                    if (value == null) {
-                        MappedStatement mappedStatement = (MappedStatement) invocation.getArgs()[0];
-                        SqlCommandType sqlCommandType = mappedStatement.getSqlCommandType();
-                        if (SqlCommandType.INSERT.equals(sqlCommandType)) {
-                            field.set(parameter, now);
-                        }
-                    }
-                }else if (FIELD_DEL_FLAG.equalsIgnoreCase(fieldName)) {
-                    Object value = field.get(parameter);
-                    if (value == null) {
-                        MappedStatement mappedStatement = (MappedStatement) invocation.getArgs()[0];
-                        SqlCommandType sqlCommandType = mappedStatement.getSqlCommandType();
-                        if (SqlCommandType.INSERT.equals(sqlCommandType)) {
-                            field.set(parameter, 0);
-                        }
-                    }
-                } else if (FIELD_UPDATE_BY.equalsIgnoreCase(fieldName)) {
-                    field.set(parameter, ShiroUtils.getUserId());
-                }else if (FIELD_UPDATE_TIME.equalsIgnoreCase(fieldName)) {
-                    field.set(parameter, now);
                 }
+            }else{
+                this.setFiled(parameter,invocation,now,userId);
             }
         } catch(ShiroException e){
             throw e;
@@ -74,6 +52,47 @@ public class MybatisInterceptor implements Interceptor {
             logger.error("Mybatis公共字段设置值时出错", e);
         }
         return invocation.proceed();
+    }
+
+    private void setFiled(Object parameter,Invocation invocation,Date now,Long userId) throws IllegalAccessException {
+        Class<?> classParameter =  parameter.getClass();
+        Field[] fields = classParameter.getDeclaredFields();
+        for (Field field : fields) {
+            field.setAccessible(true);
+            String fieldName = field.getName();
+            if (FIELD_CREATE_BY.equalsIgnoreCase(fieldName)) {
+                Object value = field.get(parameter);
+                if (value == null) {
+                    MappedStatement mappedStatement = (MappedStatement) invocation.getArgs()[0];
+                    SqlCommandType sqlCommandType = mappedStatement.getSqlCommandType();
+                    if (SqlCommandType.INSERT.equals(sqlCommandType)) {
+                        field.set(parameter, userId);
+                    }
+                }
+            }else if (FIELD_CREATE_TIME.equalsIgnoreCase(fieldName)) {
+                Object value = field.get(parameter);
+                if (value == null) {
+                    MappedStatement mappedStatement = (MappedStatement) invocation.getArgs()[0];
+                    SqlCommandType sqlCommandType = mappedStatement.getSqlCommandType();
+                    if (SqlCommandType.INSERT.equals(sqlCommandType)) {
+                        field.set(parameter, now);
+                    }
+                }
+            }else if (FIELD_DEL_FLAG.equalsIgnoreCase(fieldName)) {
+                Object value = field.get(parameter);
+                if (value == null) {
+                    MappedStatement mappedStatement = (MappedStatement) invocation.getArgs()[0];
+                    SqlCommandType sqlCommandType = mappedStatement.getSqlCommandType();
+                    if (SqlCommandType.INSERT.equals(sqlCommandType)) {
+                        field.set(parameter, 0);
+                    }
+                }
+            } else if (FIELD_UPDATE_BY.equalsIgnoreCase(fieldName)) {
+                field.set(parameter, userId);
+            }else if (FIELD_UPDATE_TIME.equalsIgnoreCase(fieldName)) {
+                field.set(parameter, now);
+            }
+        }
     }
 
     @Override
